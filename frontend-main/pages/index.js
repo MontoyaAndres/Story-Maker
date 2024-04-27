aab1b2e3ff00aa5f81d96443dadb6470393e3991
MontoyaAndres <andresmontoyafcb@gmail.com>
2024-04-27 11:16:38 -0500

    Feat: frontend-main

--- frontend-main/pages/index.js
+++ frontend-main/pages/index.js
@@ -1,5 +1,20 @@
+import { Typography } from "@mui/material";
+
+import { Wrapper } from "../styles";
+
 const Home = () => {
-  return <h1>hey</h1>;
+  return (
+    <Wrapper>
+      <div className="titles">
+        <Typography variant="h4" className="title">
+          Welcome to Story-Maker
+        </Typography>
+        <Typography variant="h6" className="description">
+          Generate a story by clicking the button below
+        </Typography>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Home;
--- frontend-main/styles.js
+++ frontend-main/styles.js
@@ -1,3 +1,24 @@
 import styled from "@emotion/styled";
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  .titles {
+    max-width: 700px;
+    margin: 0 auto;
+  }
+
+  .title {
+    margin: 20px auto;
+    text-align: center;
+    margin-bottom: 10px;
+    font-weight: 600;
+  }
+
+  .description {
+    text-align: center;
+    margin-top: 10px;
+  }
+`;
